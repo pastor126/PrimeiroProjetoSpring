9a5cc18ea3ad180fea3506519de22365f2b72847
Eduardo C Pacheco <pastor126@gmail.com>
2023-01-30 11:25:18 -0400

    Primeiro commit springboot.

---
+++ src/main/java/Edu/PrimeiroProjetoSpring/PrimeiroProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package Edu.PrimeiroProjetoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringApplication.class, args);
+	}
+
+}
