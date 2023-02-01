111c784c1469ee9390db7234478f2c94ad6c277e
Eduardo C Pacheco <pastor126@gmail.com>
2023-02-01 18:08:32 -0400

    Configuração para que as transações com o banco de dados se mantenham na camada de serviços e não passe para a camada de controle (resource).

--- src/main/java/Edu/PrimeiroProjetoSpring/services/CategoryService.java
+++ src/main/java/Edu/PrimeiroProjetoSpring/services/CategoryService.java
@@ -6,6 +6,7 @@ import Edu.PrimeiroProjetoSpring.repositories.CategoryRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -17,6 +18,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
     
+    @Transactional(readOnly = true)
     public List<Category>findAll() {
        return repository.findAll();
     }
