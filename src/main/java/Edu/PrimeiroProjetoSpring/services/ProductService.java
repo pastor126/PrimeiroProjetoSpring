3d2dc1bb06c5852bca58e0a0871a874a8a07b305
Eduardo C Pacheco <pastor126@gmail.com>
2023-02-01 19:34:57 -0400

    Configuração DTO para que as transações com o banco de dados se mantenham na camada de serviços e não passe para a camada de controle (resource).

--- src/main/java/Edu/PrimeiroProjetoSpring/services/ProductService.java
+++ src/main/java/Edu/PrimeiroProjetoSpring/services/ProductService.java
@@ -1,8 +1,11 @@
 
 package Edu.PrimeiroProjetoSpring.services;
 
+import Edu.PrimeiroProjetoSpring.dto.ProductDTO;
 import Edu.PrimeiroProjetoSpring.entities.Product;
 import Edu.PrimeiroProjetoSpring.repositories.ProductRepository;
+import java.util.ArrayList;
+import static java.util.Collections.list;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,7 +21,16 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
     @Transactional(readOnly = true)
-    public List<Product>findAll() {
-       return repository.findAll();
+    public List<ProductDTO>findAll() {
+       List<Product> list = repository.findAll();
+       List<ProductDTO> listDTO = new ArrayList<>();
+       for(Product pdt : list){
+           listDTO.add(new ProductDTO(pdt));
+       }
+       return listDTO;
+    }
+
+    public Object findById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
