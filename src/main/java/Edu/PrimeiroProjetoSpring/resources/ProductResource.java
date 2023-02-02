227086fac5f3237ebf889e30090d118ccb7a8045
Eduardo C Pacheco <pastor126@gmail.com>
2023-02-02 16:45:53 -0400

    Criado método para atualizar os dados.

--- src/main/java/Edu/PrimeiroProjetoSpring/resources/ProductResource.java
+++ src/main/java/Edu/PrimeiroProjetoSpring/resources/ProductResource.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -77,6 +78,12 @@ public class ProductResource {
        return ResponseEntity.created(uri).body(dto);
     }
     
+    // @PutMapping para atualização (update) recebendo o valor do id.    
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
 
 
     
